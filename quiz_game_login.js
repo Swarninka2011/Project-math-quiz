39d6469955a6fd82aa71adcaf196e1d673d409e0
Swarninka2011 <79652086+Swarninka2011@users.noreply.github.com>
2021-08-10 16:16:25 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -5,3 +5,9 @@ localStorage.setItem("player_1" , player_1);
 localStorage.setItem("player_2" , player_2);
 window.location = "index.html";
 }
+
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    answer = parseInt(number1) * parseInt(number2);
+}
